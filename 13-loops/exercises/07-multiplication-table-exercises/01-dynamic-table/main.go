207933cd36c8b708ea8654cd2b437ded5d86462f
shanmugarajan-93 <57755619+shanmugarajan-93@users.noreply.github.com>
2019-12-12 12:00:53 +0530

    Update main.go

--- 13-loops/exercises/07-multiplication-table-exercises/01-dynamic-table/main.go
+++ 13-loops/exercises/07-multiplication-table-exercises/01-dynamic-table/main.go
@@ -8,6 +8,13 @@
 
 package main
 
+ import ( 
+           "fmt"
+            "os"
+            "strconv"
+ )
+
+
 // ---------------------------------------------------------
 // EXERCISE: Dynamic Table
 //
@@ -54,4 +61,32 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+   
+   const size = 5
+  
+  args:= os.Args
+  
+  if len(args)!=2{
+    fmt.Println("Give a Right Table")
+      return
+  }
+  
+  size,err:= strconv.Atoi(args[1])
+    
+  if err != nil && size <= 0 {
+    fmt.Println("Wrong Table")
+  }
+  fmt.Printf("%5s","X")
+  
+  for i:=0; i <= size; i++ {
+    
+    fmt.Printf("%5d",i)
+    
+    for j:=0; j <= size; j++ {
+      
+      fmt.Printf("%5d",i*j)
+    }
+    fmt.Println()
+}
+  fmt.Println()
 }
